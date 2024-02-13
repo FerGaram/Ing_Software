536981c0460bff9786de262cbca3daebcdeadea3
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2024-02-12 23:55:44 -0600

    Base establecida del cliente

--- cliente/gameverse/src/app/app.module.ts
+++ cliente/gameverse/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsuariosComponent } from './components/usuarios/usuarios.component';
+import { LoginComponent } from './components/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsuariosComponent
+    UsuariosComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ cliente/gameverse/src/app/components/login/login.component.ts
@@ -0,0 +1,51 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/models/Usuario';
+import { UsuarioService } from 'src/app/services/usuarios.service';
+import Swal from 'sweetalert2';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  usuario = new Usuario() ;
+  constructor(private usuarioService : UsuarioService , private router: Router){
+
+  }
+  
+  logueo()
+  {
+    this.usuarioService.existe(this.usuario.correo,this.usuario.password).subscribe((resusuario: any) =>
+    {
+      console.log(resusuario);
+      if(resusuario.id != -1)
+      {
+        console.log("Usuario valido");
+        if (resusuario.esAdmin) {
+          console.log("Usuario es administrador");
+          localStorage.setItem('correo', resusuario.correo);
+          localStorage.setItem('id', resusuario.id);
+          this.router.navigateByUrl('/homeAdmin/usuarios');
+        } else {
+          console.log("Usuario no es administrador, procediendo con el inicio de sesión");
+          localStorage.setItem('correo', resusuario.correo);
+          localStorage.setItem('id', resusuario.id);
+          this.router.navigateByUrl('/home/ofertas');
+        }
+      } else {
+        console.log("Error, Usuario o contraseña incorrectos");
+        Swal.fire({
+          title: "ERROR!",
+          text: "Usuario o contraseña incorrectos!",
+          icon: "warning"
+        });
+      }
+    }, err => console.error(err));
+  }
+
+  
+}
+
+
