b76820a2603f830c68909363f6cecf0c21378b8b
FridaMartinezLorenzo <117133767+FridaMartinezLorenzo@users.noreply.github.com>
2024-02-13 14:19:31 -0600

    Añadiendo componente de productos

--- cliente/gameverse/src/app/components/productos/productos.component.ts
+++ cliente/gameverse/src/app/components/productos/productos.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { ProductosService} from './../../services/productos.service';
+import { Producto } from 'src/app/models/Productos';
+import Swal from 'sweetalert2';
+declare var M: any;
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
@@ -7,9 +11,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductosComponent implements OnInit {
 
-  constructor() { }
+  producto = new Producto() ;
+  listaProducto: any[] = [];
+  productoBuscado: any;
+  productoEliminado: any;
 
-  ngOnInit(): void {
+  constructor(private productosService : ProductosService ,private router: Router) { }
+
+  ngOnInit() {
+
+    this.listar();
+  }
+  listar(){
+    this.productosService.list().subscribe((respro: any) =>
+      {
+        this.listaProducto = respro;
+      },
+      err => console.error(err)
+      );
+      console.log(this.listaProducto);
   }
 
 }
