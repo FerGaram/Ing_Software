26ed8fb0a06b668db08e1123354be5b806ee5290
Samuel-droid-hue <gocs020101@gs.utm.mx>
2024-02-13 21:35:24 -0600

    Modal para mostrar un juego especifico

--- server/src/build/keys.js
+++ server/src/build/keys.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     database: {
         host: "localhost",
-        user: "root",
-        password: "",
+        user: "ismael",
+        password: "123",
         database: "gameverse"
     }
 };
